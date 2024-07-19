373d5c54340814f03b7bd40329b2eac77fca5ec5
박준현 <gktkwk91@naver.com>
2024-07-20 00:39:31 +0900

    User클래스 구현

--- src/main/java/com/exam/gitstudy/User.java
+++ src/main/java/com/exam/gitstudy/User.java
@@ -1,4 +1,15 @@
 package com.exam.gitstudy;
 
 public class User {
+    private String id;
+    private String name;
+    private String password;
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+    public String getId() {
+        return id;
+    }
 }
